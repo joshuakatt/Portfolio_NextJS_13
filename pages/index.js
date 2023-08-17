a77599bc011fea014445a288019685ab3e042617
Joshua Kattapuram <70146035+joshuakatt@users.noreply.github.com>
2023-08-17 17:37:18 -0230

    testing: ssr

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 
 import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
-import Head from 'next/head';
+
 
 const Home = () => (
   <div className="bg-primary-black overflow-hidden">

ace21d4e8a618e636f04ebac0ef8b756febe9e65
Joshua Kattapuram <70146035+joshuakatt@users.noreply.github.com>
2023-08-17 13:09:31 -0230

    updated ssr

--- pages/projects.js
+++ pages/projects.js
@@ -1,4 +1,4 @@
-'use client'
+
 import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 import HeroProjects from '../sections/HeroProjects';

7a5f21a197b0837d82a9ccad9752bac3235cc521
Joshua Kattapuram <70146035+joshuakatt@users.noreply.github.com>
2023-08-18 01:20:14 -0230

    updated navbar drawers

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { motion } from 'framer-motion';
 import { useRouter } from 'next/router';
-import { Button, Drawer, DrawerBody, DrawerFooter, DrawerCloseButton, DrawerHeader, DrawerOverlay, DrawerContent, useDisclosure } from '@chakra-ui/react'; // Updated imports
+import { Button, Drawer, DrawerBody, DrawerFooter, DrawerCloseButton, 
+  DrawerHeader, DrawerOverlay, DrawerContent, useDisclosure, 
+  Badge, MenuDivider } from '@chakra-ui/react'; // Updated imports
 import { HamburgerIcon } from '@chakra-ui/icons';
 import styles from '../styles';
 import { navVariants } from '../utils/motion';
@@ -53,14 +55,19 @@ const Navbar = () => {
             size='xs' // Setting drawer size to extra small
           >
             <DrawerOverlay />
-            <DrawerContent bg="black">
+            <DrawerContent bg="dark-gray">
               <DrawerCloseButton />
-              <DrawerHeader>Navigate</DrawerHeader>
+              <DrawerHeader colorScheme='white'>Navigate</DrawerHeader>
               <DrawerBody>
                 <Button colorScheme='orange' onClick={() => navigateToPage('/')}>Home</Button>
                 <Button colorScheme='orange' onClick={() => navigateToPage('/projects')}>Projects</Button>
                 <Button colorScheme='orange' onClick={() => navigateToPage('/profdev')}>Professional Development</Button>
+                | <Badge variant='subtle' colorScheme='gray'> <a href='https://github.com/joshuakatt' target="_blank" rel="noopener noreferrer">Github</a></Badge>
+                | <Badge variant='solid' colorScheme='blue'> <a href='https://www.linkedin.com/in/joshua-kattapuram/' target="_blank" rel="noopener noreferrer">LinkedIn</a></Badge>
+                | <Badge variant='solid' colorScheme='purple'> <a href='mailto:jkattapuram@mun.ca' target="_blank" rel="noopener noreferrer">E-mail</a></Badge>
               </DrawerBody>
+              <DrawerHeader colorScheme='white'>Socials</DrawerHeader>
+              
             </DrawerContent>
           </Drawer>
         </div>

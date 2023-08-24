5e55a5e6ac58d6cd269eeab045e4a76dd8d86df4
Joshua Kattapuram <70146035+joshuakatt@users.noreply.github.com>
2023-08-24 01:18:52 -0230

    filter feature added to projects section.

--- sections/Projects.jsx
+++ sections/Projects.jsx
@@ -3,6 +3,7 @@ import { Accordion, AccordionItem, AccordionButton, Box, AccordionIcon, Accordio
 import styles from '../styles';
 import { staggerContainer } from '../utils/motion';
 import { exploreProjects } from '../constants';
+import React, { useState } from 'react';
 
 const Projects = () => {
   const projects = exploreProjects;
@@ -17,8 +18,76 @@ const Projects = () => {
     </motion.div>
   );
 
+  const techStacks = new Set();
+  exploreProjects.forEach(project => {
+    project.sections.forEach(section => {
+      if (section.title === "Tech-stack") {
+        section.content.split(', ').forEach(tech => techStacks.add(tech));
+      }
+    });
+  });
+
+  const [filteredProjects, setFilteredProjects] = useState(exploreProjects);
+  const [selectedTechs, setSelectedTechs] = useState(new Set());
+
+  const filterProjects = (selectedTechs) => {
+    const filtered = exploreProjects.filter(project =>
+      Array.from(selectedTechs).some(tech =>
+        project.sections.some(section =>
+          section.title === "Tech-stack" && section.content.includes(tech)
+        )
+      )
+    );
+    setFilteredProjects(filtered);
+  };
+
+  const handleTechSelection = (tech) => {
+    const newSelectedTechs = new Set(selectedTechs);
+    if (newSelectedTechs.has(tech)) {
+      newSelectedTechs.delete(tech);
+    } else {
+      newSelectedTechs.add(tech);
+    }
+    setSelectedTechs(newSelectedTechs);
+    filterProjects(newSelectedTechs);
+  };
+
+  const clearFilters = () => {
+    setSelectedTechs(new Set());
+    setFilteredProjects(exploreProjects);
+  };
+
   return (
     <section className={`${styles.paddings} relative z-10`} style={{ color: 'white', fontFamily: 'Arial, sans-serif' }}>
+      <Accordion defaultIndex={[0]} allowMultiple className='mb-24'>
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box as="span" flex='1' textAlign='left'>
+                Filter by Tech Stack
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            <div className="tech-stack-filter flex flex-wrap justify-center gap-2 mb-4">
+              {Array.from(techStacks).map(tech => (
+                <button
+                  onClick={() => handleTechSelection(tech)}
+                  className={`px-2 py-1 rounded ${selectedTechs.has(tech) ? 'bg-blue-200 hover:bg-blue-300' : 'bg-gray-200 hover:bg-gray-300'} text-black`}
+                >
+                  {tech}
+                </button>
+              ))}
+            </div>
+            <div className="clear-filter flex justify-center mb-4">
+              <button onClick={clearFilters} className="px-2 py-1 bg-red-200 hover:bg-red-300 text-black rounded">
+                Clear Filter
+              </button>
+            </div>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
       <motion.div
         variants={staggerContainer}
         initial="hidden"
@@ -26,7 +95,7 @@ const Projects = () => {
         viewport={{ once: false, amount: 0.25 }}
         className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
       >
-        {projects.map((project, index) => (
+        {filteredProjects.map((project, index) => (
           <motion.div key={index} id={project.id} className="flex lg:flex-row flex-col gap-8 mb-24 text-white"
             initial={{ x: '100%' }} animate={{ x: 0 }} transition={{ duration: 1.0, type: 'spring', stiffness: 70 }}>
             <div className="flex flex-col items-center">
